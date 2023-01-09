33e03b17c61b289d54a8520a8e426a7b1882b360
dabruhv <78553141+dabruhv@users.noreply.github.com>
2023-01-09 09:49:52 -0700

    Update Search.cpp

--- Search.cpp
+++ Search.cpp
@@ -6,16 +6,20 @@ int main() {
 	bool found = false;
 	location = 5;
 	int IDs[] = { 234, 567, 321, 567, 234, 789};
-
+	int counter = 0;
+	//create a variable for how many items you've found
 	int searchItem;
 	cout << "enter ID you are searching for:" << endl;
 	cin >> searchItem;
 
-	while (location >= sizeof(IDs[5]) / sizeof(IDs) && !found) {
+	while (location >= 0 ) {
 		cout << "slot is " << location << " which holds "<<IDs[location]<<endl;
 		if (IDs[location] == searchItem) {
 			cout << "found is true!" << searchItem << " found in slot " << location << endl;
 			found = true;
+			counter++;
+			location--;
+			//add to that varibale
 		}
 		else
 			location--;
@@ -23,9 +27,8 @@ int main() {
 
 	if (found == true) {
 		cout << "Id " << searchItem << " was found in slot " << location << "." << endl;
+		cout << "Id " << searchItem << " was found " << counter << " times." << endl;
+		//say how many you've found
 	}
 	else
 		cout << "ID was not found." << endl;
-
-	
-}
